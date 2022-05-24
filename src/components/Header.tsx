8e372e102255b84015ad74ead458a2ef5880c899
MalenkovDN <m18dm.dm@gmail.com>
2022-05-24 16:23:16 +0300

    add style in index css

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,6 +5,11 @@ export default function Header() {
     <header>
         <div>
             <span className="logo">House staff</span>
+            <ul className="nav">
+              <li>About</li>
+              <li>contacts</li>
+              <li>login</li>
+            </ul>
         </div>
         <div className="presentation"></div>
     </header>
